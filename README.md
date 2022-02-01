064208c1229408e6ad027d4073086d5222cba39c
chih-peng.chang <chih-peng.chang@directpark.de>
2022-02-01 09:13:23 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# opengel_practice
---
+++ src/ch6.2_more_attribute.cpp
@@ -0,0 +1,239 @@
+#include "glad/glad.h"
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+const char *vertexShaderSrc =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 setColor;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   setColor =aColor;\n"
+    "}\0";
+const char *fragShaderSrc = "#version 330 core\n"
+                            "in vec3 setColor;\n"
+                            "out vec4 FragColor;\n"
+                            "void main()\n"
+                            "{\n"
+                            "   FragColor = vec4(setColor, 1.0);\n"
+                            "}\n\0";
+
+std::vector<unsigned int> vbos{};
+std::vector<unsigned int> vaos{};
+std::vector<unsigned int> ebos{};
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+void init_glfw() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+}
+
+GLFWwindow *create_window() {
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    exit(-1);
+  }
+  glfwMakeContextCurrent(window);
+  return window;
+}
+
+void load_glad() {
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    exit(-1);
+  }
+}
+
+// build and compile our shader program
+unsigned int create_shader_program() {
+  int success;
+  char infoLog[512];
+  auto create_shader = [&success, &infoLog](auto shader_type,
+                                            const char *shader_src) {
+    unsigned int shader = glCreateShader(shader_type);
+    glShaderSource(shader, 1, &shader_src, NULL);
+    glCompileShader(shader);
+    // check for shader compile errors
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(shader, 512, NULL, infoLog);
+      std::cout << "ERROR: SHADER COMPILATION_FAILED\n\tsource:\n\t"
+                << shader_src << infoLog << std::endl;
+    }
+    return shader;
+  };
+  auto create_shader_program =
+      [&success,
+       &infoLog](std::vector<unsigned int> const &shaders) -> unsigned int {
+    unsigned int shaderProgram = glCreateProgram();
+    std::for_each(shaders.begin(), shaders.end(),
+                  [&shaderProgram](auto const &shader) {
+                    glAttachShader(shaderProgram, shader);
+                  });
+
+    // link shaders
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                << infoLog << std::endl;
+    }
+
+    std::for_each(shaders.begin(), shaders.end(),
+                  [](auto const &shader) { glDeleteShader(shader); });
+
+    return shaderProgram;
+  };
+
+  unsigned int vtxShader = create_shader(GL_VERTEX_SHADER, vertexShaderSrc);
+  unsigned int fragShader = create_shader(GL_FRAGMENT_SHADER, fragShaderSrc);
+  return create_shader_program({vtxShader, fragShader});
+}
+
+unsigned int create_vao() {
+  float vertices[] = {
+      // positions // colors
+      0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+      0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f  // top
+
+  };
+
+  unsigned int VBO, VAO;
+
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
+  // then configure vertex attributes(s).
+
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // layout num, type,xxx  ,stride, offset
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // note that this is allowed, the call to glVertexAttribPointer registered VBO
+  // as the vertex attribute's bound vertex buffer object so afterwards we can
+  // safely unbind
+  vaos.push_back(VAO);
+  vbos.push_back(VBO);
+  return VAO;
+}
+
+unsigned int create_ebo(int vao) {
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 2, // first Triangle
+               // 1, 2, 3  // second Triangle
+  };
+
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glBindVertexArray(vao);
+  // remember: do NOT unbind the EBO while a VAO is active as the bound element
+  // buffer object IS stored in the VAO; keep the EBO bound.
+  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+  // You can unbind the VAO afterwards so other VAO calls won't accidentally
+  // modify this VAO, but this rarely happens. Modifying other VAOs requires a
+  // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
+  // VBOs) when it's not directly necessary.
+
+  return EBO;
+}
+
+void clean_buffer() {
+  while (!vaos.empty()) {
+    glDeleteVertexArrays(1, &vaos.back());
+    vaos.pop_back();
+  }
+
+  while (!vbos.empty()) {
+    glDeleteBuffers(1, &vbos.back());
+    vbos.pop_back();
+  }
+  while (!ebos.empty()) {
+    glDeleteBuffers(1, &ebos.back());
+    ebos.pop_back();
+  }
+}
+
+int main() {
+
+  init_glfw();
+
+  auto window = create_window();
+
+  load_glad();
+
+  auto shader_program = create_shader_program();
+
+  // prepare data
+  auto VAO = create_vao();
+  auto EBO = create_ebo(VAO);
+
+  // Render loop
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  std::cout << "press [Esc] to close the window" << std::endl;
+  while (!glfwWindowShouldClose(window)) {
+    // process input
+    processInput(window);
+
+    // render
+    // ------
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw our first triangle
+    glUseProgram(shader_program);
+    glBindVertexArray(VAO);
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(0); // no need to unbind it every time
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
+
+    // Check and call events and swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  clean_buffer();
+  glDeleteProgram(shader_program);
+  // Close
+  glfwTerminate();
+
+  return 0;
+}
